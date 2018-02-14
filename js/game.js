fa4c86a7a971ada71d10529b7098042aabb58f53
Tristan Canova <tcanova1@gmail.com>
2018-02-14 14:17:53 -0500

    starting to make groups and collision groups for bullets and players to be able to interact

--- js/game.js
+++ js/game.js
@@ -3,8 +3,9 @@
  * E-mail: tcanova1@gmail.com
  */
 
-var Game = {};
+///CURRENTLY WORKING ON BULLET SHIT
 
+var Game = {};
 Game.init = function(){
     game.stage.disableVisibilityChange = true;
 };
@@ -13,6 +14,7 @@ Game.preload = function() {
     game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
     game.load.image('sprite','assets/sprites/ship.png');
+    game.load.spritesheet('bullet', 'assets/sprites/rgblaser.png', 4, 4);
     cursors = game.input.keyboard.createCursorKeys();
 };
 
@@ -30,7 +32,26 @@ Game.create = function(){
     }
     me.game.physics.startSystem(Phaser.Physics.P2JS);
     me.game.physics.p2.gravity.y = 0;
+    //Turn on impact events for the world, without this we get no collision callbacks
+    me.game.physics.p2.setImpactEvents(true);
+    me.game.physics.p2.restitution = 0.8;
+    me.game.physics.p2.updateBoundsCollisionGroup();
+   
     Game.createdPlayer = false;
+    
+    var playerCollisionGroup = game.physics.p2.createCollisionGroup();
+    var bulletCollisionGroup = game.physics.p2.createCollisionGroup();
+    
+    var bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.P2JS;
+    
+    
+    
+    
+    
+    
+    
     Client.askNewPlayer();
     
 };
@@ -44,7 +65,7 @@ Game.update = function(){
         if(cursors.up.isDown){Game.player.body.thrust(200);}
         else if(cursors.down.isDown){Game.player.body.reverse(100);}
         //console.log(Game.player.body.rotation);
-        Client.movePlayer({x: Game.player.body.x, y: Game.player.body.y, angle: Game.player.body.rotation});//WORKING ON THIS NEXT
+        Client.movePlayer({x: Game.player.body.x, y: Game.player.body.y, angle: Game.player.body.rotation});
     }
 };
 
@@ -66,6 +87,7 @@ Game.createPlayer = function(id,x,y,angle){
     Game.createdPlayer = true;
     Game.player = game.add.sprite(x,y,'sprite');
     game.physics.p2.enableBody(Game.player, true);
+    game.camera.follow(Game.player);
     console.log("create player with location:"+ x + " ," + y);
 };
 
